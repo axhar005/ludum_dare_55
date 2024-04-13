aef51b75e5e7e568f7009ed1b198e7866b5106e7
sm222 <soleil.maraska@gmail.com>
2024-04-13 16:30:02 -0400

    working on render

--- antoine/ImageFormat.cpp
+++ antoine/ImageFormat.cpp
@@ -3,13 +3,22 @@
 void	InitMapLayer(Layer &map) {
 	if (map.empty()) {
 		for (int i = 0; i < LAYER_NUMBER; i++) {
-			map.insert( std::make_pair(i, vector<t_ImageFormat>()));
+			map.insert(std::make_pair(i, vector<t_ObjFormat>()));
 		}
 	}
 }
 
-void	AddImageFormatToLayer(Layer& input, int y, t_ImageFormat& img) {
+void	AddImageFormatToLayer(Layer& input, int y, t_ObjFormat& img) {
 	if (y < 0 || y > LAYER_NUMBER)
 		return;
 	input[y].push_back(img);
+}
+
+void	render(Layer& layer) {
+	for (int i = 0; i < LAYER_NUMBER; i++) {
+		for (size_t j = 0; j < layer[1].size(); j++) {
+			t_ObjFormat& tmp = layer[i][j];
+			DrawTexture(tmp.img, tmp.vec2.x, tmp.vec2.y, WHITE);
+		}
+	}
 }
--- antoine/ImageFormat.hpp
+++ antoine/ImageFormat.hpp
@@ -3,19 +3,23 @@
 #include <raylib.h>
 #include <map>
 #include <vector>
+#include <unistd.h>
 
 using std::map;
 using std::vector;
 
 #define LAYER_NUMBER 6
 
-typedef struct s_ImageFormat
+typedef struct s_ObjFormat
 {
 	Vector2		vec2;
 	Texture2D	img;
-}	t_ImageFormat;
+	//
+	// if you need to add data to the obj add here
+	//
+}	t_ObjFormat;
 
-typedef map<int , vector<t_ImageFormat> > Layer;
+typedef map<int , vector<t_ObjFormat> > Layer;
 
 void	InitMapLayer(Layer &map);
-void	AddImageFormatToLayer(Layer& input, int y, t_ImageFormat& img);
+void	AddImageFormatToLayer(Layer& input, int y, t_ObjFormat& img);
