659dd29c097e82f86b7265615d2daf9bae26093a
sm222 <soleil.maraska@gmail.com>
2024-04-13 15:27:45 -0400

    working on game obg

--- antoine/ImageFormat.cpp
+++ antoine/ImageFormat.cpp
@@ -1,4 +1,15 @@
 #include "ImageFormat.hpp"
 
+void	InitMapLayer(Layer &map) {
+	if (map.empty()) {
+		for (int i = 0; i < LAYER_NUMBER; i++) {
+			map.insert( std::make_pair(i, vector<t_ImageFormat>()));
+		}
+	}
+}
+
 void	AddImageFormatToLayer(Layer& input, int y, t_ImageFormat& img) {
+	if (y < 0 || y > LAYER_NUMBER)
+		return;
+	input[y].push_back(img);
 }
--- antoine/ImageFormat.hpp
+++ antoine/ImageFormat.hpp
@@ -7,10 +7,12 @@
 using std::map;
 using std::vector;
 
+#define LAYER_NUMBER 6
+
 typedef struct s_ImageFormat
 {
 	Vector2		vec2;
 	Texture2D	img;
 }	t_ImageFormat;
 
-typedef map<int , vector<Texture2D> > Layer;
+typedef map<int , vector<t_ImageFormat> > Layer;
